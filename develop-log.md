a27a56e224bb6f2aadc7485933e3bc8dcd92372d
pagnkelly <632228198@qq.com>
2022-08-23 17:31:54 +0800

    feat: index i

--- develop-log.md
+++ develop-log.md
@@ -7,3 +7,7 @@
 ## 2022.08.23
 
 * 建立package.json，规划目录，mpx采用lerna管理包，我们不太关注这一方面，所以就不添加lerna了
+
+* setup bable,jest,ts
+
+* 看core中的index导出,有`createApp`,`createPage`,`createComponent`,`watch`,`observable`，还有默认的mpx对象`EXPORT_MPX`,其他的一些工具函数`toPureObject`,`包括ts的store导出函数`就先不实现了
---
+++ packages/core/src/index.ts
@@ -0,0 +1,20 @@
+export function createApp () {
+}
+
+export function createPage () {
+}
+
+export function createComponent () {
+}
+
+let observable
+let watch
+
+export { watch, observable }
+function factory() {
+  return {}
+}
+
+const EXPORT_MPX = factory()
+
+export default EXPORT_MPX
